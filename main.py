b8ea46216d733e781be9f27f22da8348e87c249d
stefanos-larkou <s.larkou@outlook.com>
2024-01-12 00:39:33 +0000

    add example usage code for all types of random walks

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
+import matplotlib.pyplot as plt
+from random_walker import RandomWalker
+
 if __name__ == "__main__":
-    pass
+    # Example Usage
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(20):
+        rwalker = RandomWalker(start=(0,), ndim=1)
+        rwalker.random_walk(1000)
+        rwalker.plot_track(ax)
+
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(20):
+        rwalker = RandomWalker(start=(0, 0), ndim=2)
+        rwalker.random_walk(1000)
+        rwalker.plot_track(ax)
+
+    plt.show()
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for i in range(20):
+        rwalker = RandomWalker(start=(0, 0, 0), ndim=3)
+        rwalker.random_walk(1000)
+        rwalker.plot_track(ax)
+
+    plt.show()
