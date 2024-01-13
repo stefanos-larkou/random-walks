e8fc730ca3ae78f6f8deb9a8b60583c9a339c995
stefanos-larkou <s.larkou@outlook.com>
2024-01-13 01:06:21 +0000

    generate animation instead of plot

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.animation import FuncAnimation
+from matplotlib import pyplot as plt
 from random_walker import RandomWalker
 
+matplotlib.use("TkAgg")
+
+REPRODUCIBLE = True
+STABLE_LIMS = True
+NDIM = 1
+START = (0,) * NDIM
+NSTEPS = 100
+SEED = [i + 1 for i in range(NSTEPS)] if REPRODUCIBLE else [-1] * NSTEPS
+NWALKERS = 20
+
+
+def update(frame, ax, rwalkers):
+    artists = []
+    ax.clear()
+
+    for rwalker in rwalkers:
+        rwalker.plot_track(ax, frame, STABLE_LIMS)
+        artists.extend(ax.lines)
+
+    return artists
+
+
 if __name__ == "__main__":
-    # Example Usage
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for i in range(20):
-        rwalker = RandomWalker(start=(0,), ndim=1)
-        rwalker.random_walk(100)
-        rwalker.plot_track(ax)
+    ax = fig.add_subplot(111, projection='3d') if NDIM == 3 else fig.add_subplot(111)
 
-    plt.show()
+    rwalkers = []
+    for i in range(NWALKERS):
+        rwalker = RandomWalker(start=START, ndim=NDIM, seed=SEED[i])
+        rwalker.random_walk(NSTEPS)
+        rwalkers.append(rwalker)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for i in range(20):
-        rwalker = RandomWalker(start=(0, 0), ndim=2)
-        rwalker.random_walk(100)
-        rwalker.plot_track(ax)
-
-    plt.show()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    for i in range(20):
-        rwalker = RandomWalker(start=(0, 0, 0), ndim=3)
-        rwalker.random_walk(100)
-        rwalker.plot_track(ax)
-
+    ani = FuncAnimation(fig, update, fargs=(ax, rwalkers,), frames=NSTEPS, interval=min(1 / NSTEPS, 0.5), repeat=False)
     plt.show()
